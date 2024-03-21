e3a033e81d4914b1e0d087236ee7b70b2bce6aef
Daniel leal <daniel.felipe.lg@hotmail.com>
2024-03-21 16:19:40 -0500

    Change of names json news

--- app/Models/News.php
+++ app/Models/News.php
@@ -23,6 +23,23 @@ class News extends Model
 
     public static function select(){
         $news = DB::table('ViewNews')->select(['new_id','new_date','new_description','new_typ_id','new_typ_name','proj_id','proj_name','use_id','use_mail','per_name'])->get();
-        return $news;
+    
+        $transformedNews = $news->map(function ($item) {
+            return [
+                'id' => $item->new_id,
+                'Fecha' => $item->new_date,
+                'Descripcion' => $item->new_description,
+                'Id novedad' => $item->new_typ_id,
+                'Nombre novedad' => $item->new_typ_name,
+                'Id proyecto' => $item->proj_id,
+                'Nombre proyecto' => $item->proj_name,
+                'Id del usuario' => $item->use_id,
+                'Correo usuario' => $item->use_mail,
+                'nombre persona' => $item->per_name,
+            ];
+        });
+    
+        return $transformedNews;
     }
+    
 }
